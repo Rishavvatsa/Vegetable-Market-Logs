ecb4e93045a5aab937321186dc51d3617fb94000
Rishav <vatsarishav99@gmail.com>
2024-01-28 20:29:09 +0530

    new

--- Frontend/src/Pages/Carousel.jsx
+++ Frontend/src/Pages/Carousel.jsx
@@ -1,6 +1,6 @@
-import React, { useState,useEffect } from 'react';
-import { slides } from '../data/data';
-import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
+import React, { useState, useEffect } from "react";
+import { slides } from "../data/data";
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
 
 const Carousel = () => {
   const [currIndex, setCurrIndex] = useState(0);
@@ -14,42 +14,42 @@ const Carousel = () => {
     const newIndex = (currIndex + 1) % slides.length;
     setCurrIndex(newIndex);
   };
+
   useEffect(() => {
-    const autoplayInterval = setInterval(nextSlide, 5000); 
+    const autoplayInterval = setInterval(nextSlide, 5000);
 
     return () => {
       clearInterval(autoplayInterval);
     };
   }, [currIndex]);
 
-
   return (
-    <div className='max-w-[1440px] h-[780px] w-full m-auto py-16 px-4 relative top-10 z-1'>
-      {slides[currIndex].type === 'image' ? (
+    <div className="max-w-screen-xl mx-auto relative py-16 px-4 md:top-10 ">
+      {slides[currIndex].type === "image" ? (
         <div
-          style={{ backgroundImage: `url(${slides[currIndex].src})` }}
-          className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
+        style={{ backgroundImage: `url(${slides[currIndex].src})`, backgroundSize: 'cover', width:'100%' }}
+          className="w-full h-96 md:h-80 lg:h-96 xl:h-[780px] rounded-full bg-center bg-cover duration-500"
         ></div>
       ) : (
-        <video className='w-full h-full rounded-2xl' autoPlay loop muted>
-          <source src={slides[currIndex].src} type='video/mp4' />
+        <video className="w-full h-96 md:h-80 lg:h-96 xl:h-[780px] rounded-2xl" autoPlay loop muted>
+          <source src={slides[currIndex].src} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       )}
 
-      <div className='absolute top-[50%] -translate-x-0 translate-y-[50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+      <div className="absolute top-[50%] -translate-x-0 translate-y-[50%] left-5 md:left-10 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
         <BsChevronCompactLeft size={30} onClick={prevSlide} />
       </div>
-      <div className='absolute top-[50%] -translate-x-0 translate-y-[50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+      <div className="absolute top-[50%] -translate-x-0 translate-y-[50%] right-5 md:right-10 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
         <BsChevronCompactRight size={30} onClick={nextSlide} />
       </div>
-      <div className='flex top-4 justify-center py-2'>
+      <div className="flex top-4 justify-center py-2">
         {slides.map((slide, slideIndex) => (
           <div
             key={slideIndex}
             onClick={() => setCurrIndex(slideIndex)}
             className={`text-2xl cursor-pointer ${
-              currIndex === slideIndex ? 'text-blue-500' : ''
+              currIndex === slideIndex ? "text-blue-500" : ""
             }`}
           >
             •
--- Frontend/src/Pages/Cart.jsx
+++ Frontend/src/Pages/Cart.jsx
@@ -55,94 +55,44 @@ const Cart = () => {
     (acc, curr) => acc + parseInt(curr.total),
     0
   );
+  
 
   const totalQty = productCartItem.reduce(
     (acc, curr) => acc + parseInt(curr.qty),
     0
   );
- 
 
-  const handlePayment = async () => {
-    if (user.email) {
-      const userId = user._id;
-      
-      console.log(userId)
-      try {
-        const stripePromise = await loadStripe(
-          "pk_test_51NhH3zSA0NxJWLDqvIt8SneTD5023LPufXUeOrwvU5qT3ttZqqZkRn0V7kEhYzzmgCttNzN8XFAZAlE9GOjpTM7h00aSFVZVeJ"
-        );
-  
-        const res = await fetch(
-          `${process.env.REACT_APP_SERVER_DOMAIN}create-checkout-session`,
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(productCartItem),
-          }
-        );
-  
-        if (res.status === 500) return;
-  
-        const data = await res.json();
-  
-        // Redirect to payment gateway
-        toast("Redirecting to payment gateway...");
-        stripePromise.redirectToCheckout({ sessionId: data }).then(result => {
-          if (result.error) {
-            // Handle error
-            console.error("Payment error:", result.error);
-          } else if (result.paymentIntent.status === "succeeded") {
-           
-            createOrder(userId,productCartItem);
-          }
-        });
-      } catch (error) {
-        console.error("Payment error:", error);
-      }
-    } else {
-      toast("You are not logged in!");
-      setTimeout(() => {
-        navigate("/login");
-      }, 1000);
-    }
-  
-  };
-  
-  const createOrder = async (user,productCart) => {
-    try {
-      const response = await fetch(
-        `${process.env.REACT_APP_SERVER_DOMAIN}api/create-order`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            products: productCart.map((item) => ({
-              user,
-              productId: item._id,
-              quantity: item.qty,
-            })),
-            totalAmount: totalPrice + shipcharge - discount,
-          }),
-          
-        }
+
+  const handlePayment = async()=>{
+
+    if(user.email){
+        
+      const stripePromise = await loadStripe(
+        "pk_test_51NhH3zSA0NxJWLDqvIt8SneTD5023LPufXUeOrwvU5qT3ttZqqZkRn0V7kEhYzzmgCttNzN8XFAZAlE9GOjpTM7h00aSFVZVeJ"
       );
-  
-      if (response.status === 201) {
-        dispatch(clearCart());
-        toast("Order created successfully!");
-      } else {
-        toast("Failed to create order.");
-      }
-    } catch (error) {
-      console.error("Error creating order:", error);
-      toast("Error creating order.");
+        const res = await fetch(`${process.env.REACT_APP_SERVER_DOMAIN}/create-checkout-session`,{
+          method : "POST",
+          headers  : {
+            "content-type" : "application/json"
+          },
+          body  : JSON.stringify(productCartItem)
+        })
+        if(res.statusCode === 500) return;
+
+        const data = await res.json()
+        console.log(data)
+
+        toast("Redirect to payment Gateway...!")
+        stripePromise.redirectToCheckout({sessionId : data}) 
     }
-  };
+    else{
+      toast("You have not Login!")
+      setTimeout(()=>{
+        navigate("/login")
+      },1000)
+    }
   
+}
  
   const handleClear = () => {
     dispatch(clearCart());
@@ -154,25 +104,18 @@ const Cart = () => {
         heading="Add to Cart"
         bannerimage="https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
       />
-     
-<div className="text-left mt-6 ">
-  <p className="text-lg font-bold text-gray-600">Add Address </p>
-  <Link to={"/address"} > 
-  <AiFillFolderAdd className="text-green-500 hover:text-purple-700 text-4xl" /> 
-  </Link>
- 
-</div>
+
+      <div className="text-left mt-6 ">
+        <p className="text-lg font-bold text-gray-600">Add Address </p>
+        <Link to={"/address"}>
+          <AiFillFolderAdd className="text-green-500 hover:text-purple-700 text-4xl" />
+        </Link>
+      </div>
       <div className="p-2 md:p-4">
         <h2 className="text-lg md:text-2xl font-bold text-slate-600">
           Your Cart Items
         </h2>
-        <div className="marquee-container">
-          <span>
-            <Marquee className="text-marquee" speed={30} pauseOnHover>
-            Apply Coupon Code ⭐⭐ESABJI99DEAL⭐⭐ to receive a ₹99 discount on purchases above ₹199.
-            </Marquee>
-          </span>
-        </div>
+        
 
         {productCartItem[0] ? (
           <div className="my-4 flex flex-col md:flex-row gap-3">
@@ -195,7 +138,9 @@ const Cart = () => {
             </div>
 
             <div className="w-full md:max-w-md md:ml-auto ">
-              <h2 className="bg-sky-600 text-center text-white p-2 text-2xl">Summary</h2>
+              <h2 className="bg-sky-600 text-center text-white p-2 text-2xl">
+                Summary
+              </h2>
               <div className="flex w-full py-2 text-lg border-b">
                 <p>Total Quantity :</p>
                 <p className="ml-auto w-32 font-bold">{totalQty}</p>
@@ -208,50 +153,31 @@ const Cart = () => {
                 </p>
               </div>
 
-              <div className="flex w-full py-2 text-lg border-b">
-                <p>Discount:</p>
-                <p className="ml-auto w-32 font-bold">
-                  - <span className="text-red-500">₹</span> {discount}
-                </p>
-              </div>
+             
 
-              <div className="flex w-full py-2 text-lg border-b">
-                <p>Shipping:</p>
-                <p className="ml-auto w-32 font-bold">
-                  <span className="text-red-500">₹</span> {shipcharge}
-                </p>
-              </div>
+              
 
               <div className="flex w-full py-2 text-lg border-b">
                 <p>Grand Total</p>
                 <p className="ml-auto w-32 font-bold">
                   <span className="text-red-500">₹</span>{" "}
-                  {totalPrice + shipcharge - discount}
+                  {totalPrice }
                 </p>
               </div>
-              <div className="flex w-full py-2">
-                <input
-                  type="text"
-                  placeholder="Enter coupon code"
-                  value={coupon}
-                  onChange={(e) => setCoupon(e.target.value)}
-                  className="border-2 p-1 w-full sm:w-1/2"
-                />
-                <button
-                  onClick={applyCoupon}
-                  className="bg-blue-500 text-white p-1 ml-2 border-2 rounded-lg"
-                >
-                  Apply Coupon
-                </button>
-              </div>
+             
               <div className="flex w-full justify-between mt-4 gap-2">
-                <button className="bg-orange-500 hover:bg-green-500 w-full  text-lg font-bold py-2 text-white" onClick={handlePayment}> 
+                <button
+                  className="bg-orange-500 hover:bg-green-500 w-full  text-lg font-bold py-2 text-white"
+                  onClick={handlePayment}
+                >
                   Checkout
                 </button>
-                <button className="bg-sky-500 hover:bg-red-500 w-full  text-lg font-bold py-2 text-white" onClick={handleClear}> 
+                <button
+                  className="bg-sky-500 hover:bg-red-500 w-full  text-lg font-bold py-2 text-white"
+                  onClick={handleClear}
+                >
                   Clear Cart
                 </button>
-                
               </div>
             </div>
           </div>
