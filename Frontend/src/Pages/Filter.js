99208c46a553be2648c69f61d29d2ff30e590090
Rishav <vatsarishav99@gmail.com>
2024-02-12 02:30:53 +0530

    new

--- Frontend/src/Pages/Filter.js
+++ Frontend/src/Pages/Filter.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { CiForkAndKnife } from "react-icons/ci";
 
-const FilterProduct = ({category,onClick,isActive}) => {
+const FilterProduct = ({ category, onClick, isActive }) => {
   return (
     <div onClick={onClick}>
-      <div className={`text-3xl p-5  rounded-full cursor-pointer ${isActive ? "bg-yellow-500 text-white" : "bg-green-500 text-white "}`}>
-        <CiForkAndKnife className={isActive ? 'font-bold':'font-extrabold'}    />
+      <div
+        className={`text-3xl p-5  rounded-full cursor-pointer ${
+          isActive ? "bg-yellow-500 text-white" : "bg-green-500 text-white "
+        }`}
+      >
+        <CiForkAndKnife className={isActive ? "font-bold" : "font-extrabold"} />
       </div>
       <p className="text-center font-medium my-1 capitalize">{category}</p>
     </div>
