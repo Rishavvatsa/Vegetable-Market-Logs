9587bce2e2191ca12e54fff7c8bb4d5701b54c69
Rishav <vatsarishav99@gmail.com>
2024-02-12 01:29:24 +0530

    new changes

--- Frontend/src/Pages/Carousel.jsx
+++ Frontend/src/Pages/Carousel.jsx
@@ -2,13 +2,16 @@ import React, { useState, useEffect } from "react";
 import { slides } from "../data/data";
 import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
 
+
 const Carousel = () => {
   const [currIndex, setCurrIndex] = useState(0);
+  
 
   const prevSlide = () => {
     const newIndex = (currIndex - 1 + slides.length) % slides.length;
     setCurrIndex(newIndex);
   };
+
 
   const nextSlide = () => {
     const newIndex = (currIndex + 1) % slides.length;
@@ -23,38 +26,43 @@ const Carousel = () => {
     };
   }, [currIndex]);
 
-  return (
-    <div className="max-w-screen-xl mx-auto relative py-16 px-4 md:top-10 ">
-      {slides[currIndex].type === "image" ? (
-        <div
-        style={{ backgroundImage: `url(${slides[currIndex].src})`, backgroundSize: 'cover', width:'100%' }}
-          className="w-full h-96 md:h-80 lg:h-96 xl:h-[780px] rounded-full bg-center bg-cover duration-500"
-        ></div>
-      ) : (
-        <video className="w-full h-96 md:h-80 lg:h-96 xl:h-[780px] rounded-2xl" autoPlay loop muted>
-          <source src={slides[currIndex].src} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-      )}
 
-      <div className="absolute top-[50%] -translate-x-0 translate-y-[50%] left-5 md:left-10 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
-        <BsChevronCompactLeft size={30} onClick={prevSlide} />
-      </div>
-      <div className="absolute top-[50%] -translate-x-0 translate-y-[50%] right-5 md:right-10 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
-        <BsChevronCompactRight size={30} onClick={nextSlide} />
-      </div>
-      <div className="flex top-4 justify-center py-2">
-        {slides.map((slide, slideIndex) => (
+  return (
+    <div className="max-w-screen-xl mx-auto py-16 px-4 md:pt-10 flex flex-col md:flex-row relative top-20">
+
+      <div className="w-full  flex flex-col items-center relative">
+        {slides[currIndex].type === "image" ? (
           <div
-            key={slideIndex}
-            onClick={() => setCurrIndex(slideIndex)}
-            className={`text-2xl cursor-pointer ${
-              currIndex === slideIndex ? "text-blue-500" : ""
-            }`}
+            style={{ backgroundImage: `url(${slides[currIndex].src})`, backgroundSize: 'cover', width: '100%', backgroundRepeat: 'no-repeat', backgroundPosition: 'center',overflow:"hidden" }}
+            className="w-full h-96 md:h-80 lg:h-96 xl:h-[780px] rounded-lg bg-center bg-cover duration-500 mb-6 md:mb-0"
           >
-            •
+
+
           </div>
-        ))}
+        ) : (
+          <video className="w-full h-96 md:h-80 lg:h-96 xl:h-[780px] rounded-2xl mb-6 md:mb-0" autoPlay loop muted>
+            <source src={slides[currIndex].src} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        )}
+
+        <div className="absolute top-1/2 -translate-x-0 transform -translate-y-1/2 left-5 md:left-10 text-white cursor-pointer">
+          <BsChevronCompactLeft size={30} onClick={prevSlide} />
+        </div>
+        <div className="absolute top-1/2 -translate-x-0 transform -translate-y-1/2 right-5 md:right-10 text-white cursor-pointer">
+          <BsChevronCompactRight size={30} onClick={nextSlide} />
+        </div>
+        <div className="flex justify-center mt-2">
+          {slides.map((slide, slideIndex) => (
+            <div
+              key={slideIndex}
+              onClick={() => setCurrIndex(slideIndex)}
+              className={`text-2xl cursor-pointer mx-1 ${currIndex === slideIndex ? "text-blue-500" : "text-gray-500"}`}
+            >
+              •
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- Frontend/src/data/data.js
+++ Frontend/src/data/data.js
@@ -1,21 +1,18 @@
-import i1 from "../assets/b1.jpeg"
-import i2 from "../assets/catergory/i11.jpeg"
-import i3 from "../assets/catergory/i3.jpg"
+import i1 from "../assets/catergory/photo-1495480137269-ff29bd0a695c.avif";
+import i2 from "../assets/catergory/gc.png";
+import i3 from "../assets/catergory/i3.jpg";
 
-export const slides=[
-    {
-        src:i1,
-        type:"image"
-        
-    },
-    {
-        src:i2,
-        type:"image"
-    },
-    {
-        src:i3,
-        type:"image"
-    }
-    
- 
-]
+export const slides = [
+  {
+    src: i1,
+    type: "image",
+  },
+  {
+    src: i2,
+    type: "image",
+  },
+  {
+    src: i3,
+    type: "image",
+  },
+];
